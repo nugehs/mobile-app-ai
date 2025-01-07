7ab2b4536a1902899d318a605ad5e9d4da4cb4ef
Segun Olumbe <11536849+nugehs@users.noreply.github.com>
2025-01-07 22:10:19 +0000

    Add navigation types

---
+++ src/navigation/types.ts
@@ -0,0 +1,17 @@
+export type RootStackParamList = {
+  Auth: undefined;
+  Main: undefined;
+  Scanner: undefined;
+};
+
+export type AuthStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  ForgotPassword: undefined;
+};
+
+export type MainTabParamList = {
+  Events: undefined;
+  Guests: undefined;
+  Profile: undefined;
+};
